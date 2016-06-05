fdc280da36aaf55eaf85c70433c1444923f89fab
metehan <mthnzbk@gmail.com>
2016-06-05 22:24:22 +0300

    Değişen videoya göre video bilgisi sinyali yayan kod eklendi.

--- pisiplayer/videoplayer.py
+++ pisiplayer/videoplayer.py
@@ -1,15 +1,34 @@
 from PyQt5.QtMultimediaWidgets import QGraphicsVideoItem
 from PyQt5.QtMultimedia import QMediaPlayer, QMediaContent
-from PyQt5.QtCore import QUrl
+from PyQt5.QtCore import QUrl, QTimer, pyqtSignal, QFile
 
 
 class Player(QGraphicsVideoItem):
+
+    subtitlePos = pyqtSignal(int)
     def __init__(self, parent=None):
         super().__init__()
         self.parent = parent
 
         self.player = QMediaPlayer()
         self.player.setVideoOutput(self)
+
+        self.timer = QTimer(self)
+        self.timer.timeout.connect(self.timerPos)
+
+        self.player.currentMediaChanged.connect(self.signalStart)
+        self.player.currentMediaChanged.connect(self.parent.subtitleitem.subtitleControl)
+
+    def signalStart(self, content):
+        srt = content.canonicalUrl().toLocalFile().split(".")
+        srt.pop(-1)
+        srt.append("srt")
+        srt = ".".join(srt)
+        if QFile.exists(srt):
+            self.timer.start(200)
+
+    def timerPos(self):
+        self.subtitlePos.emit(self.player.position())
 
     def playerPlayOrOpen(self, arg=None):
         if type(arg) == list and len(arg) > 1:
