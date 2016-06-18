7ccca7d6065986fc00fab95a5081e919ae79f226
metehan <mthnzbk@gmail.com>
2016-06-19 01:15:51 +0300

    basit download yöneticisi

--- pisiplayer/downloadmanager.py
+++ pisiplayer/downloadmanager.py
@@ -1,5 +1,22 @@
-from PyQt5.QtNetwork import QNetworkAccessManager, QNetworkReply, QNetworkRequest
+from PyQt5.QtNetwork import QNetworkAccessManager, QNetworkRequest
+from PyQt5.QtCore import QUrl
 
 
 class DownloadManager(QNetworkAccessManager):
-    pass
+    def __init__(self, parent=None):
+        super().__init__(parent)
+
+
+    def addUrl(self, url):
+        print(url)
+        self.request = QNetworkRequest(QUrl(url))
+        self.request.setRawHeader(b"User-Agent", b"Mozilla/5.0 (Windows NT 6.1; WOW64; rv:40.0) Gecko/20100101 Firefox/40.1")
+
+        self.reply = self.get(self.request)
+
+        self.reply.downloadProgress.connect(self.setProgress)
+
+        return self.request
+
+    def setProgress(self, a, s):
+        print(a, s)
