c4c6e4b5d40188489abd5a50bbd03e74337a12ec
metehan <mthnzbk@gmail.com>
2016-06-20 14:29:04 +0300

    oluşacak hataları gideremeyeceğimden youtube_dl modülü kullanılarak yapılandırıldı.

--- pisiplayer/youtube.py
+++ pisiplayer/youtube.py
@@ -1,43 +1,70 @@
-from urllib.request import urlopen, urlparse
-from urllib.parse import parse_qs
+from youtube_dl import YoutubeDL
 
 
 class Youtube(object):
     def __init__(self, url):
 
-        _videourl = urlparse(url)
-        _videoid = _videourl.query[2:]
+        _youtube = YoutubeDL()
+        self._videourl = _youtube.extract_info(url, download=False)
 
-        self._get_video_info =  parse_qs(urlopen("https://www.youtube.com/get_video_info?video_id={}".format(_videoid)).read().decode("utf-8"))
-        self._stream_map = self._get_video_info["url_encoded_fmt_stream_map"][0].split(",")
 
     def video_list(self):
         videos = []
-        for i in self._stream_map:
+        for item in self._videourl["formats"]:
             video = {}
-            video["url"] = parse_qs(i)["url"][0]
-            video["quality"] = parse_qs(i)["quality"][0]
-            video["type"] = parse_qs(i)["type"][0].split(";")[0]
-            videos.append(video)
+            if item["format_note"] in ("medium", "hd720"):
+                video["url"] = item["url"]
+                video["ext"] = item["ext"]
+                video["format"] = item["format_note"]
+                #video[""] = item[""]
+                videos.append(video)
 
         return videos
 
+    def get_video_formats(self):
+        formats = []
+        for i in self.video_list():
+            formats.append(i["format"])
+
+        return formats
+
+    def get_video_exts(self):
+        exts = []
+        for i in self.video_list():
+            exts.append(i["ext"])
+
+        return exts
+
+    def get_video(self, quality="hd720"):
+        if quality in self.get_video_formats():
+            for i in self.video_list():
+                if i["format"] == quality:
+                    return i["url"]
+        else:
+            pass
 
     def video_id(self):
-        return self._get_video_info["video_id"][0]
+        return self._videourl["id"]
 
     def video_title(self):
-        return self._get_video_info["title"][0]
+        return self._videourl["title"]
 
     def video_author(self):
-        return self._get_video_info["author"][0]
+        return self._videourl["uploader_id"]
 
     def has_video_cc(self):
-        return self._get_video_info["has_cc"][0]
+        return self._videourl["requested_subtitles"]
+
+    def subtitles(self):
+        return self._videourl["subtitles"]
 
 
 if __name__ == "__main__":
-    you = Youtube("https://www.youtube.com/watch?v=gvVHSndpkEg")
-    print(you.video_id(), you.video_title(), you.video_author(), you.has_video_cc(), you.video_list())
+    you = Youtube("https://www.youtube.com/watch?v=IEroLrjqFVc")
+    print(you.video_id(), you.video_title(), you.video_author(), you.has_video_cc(), you.subtitles())
+    for i in you.video_list():
+        print(i)
     you = Youtube("https://www.youtube.com/watch?v=NkceiLLNtao")
-    print(you.video_id(), you.video_title(), you.video_author(), you.has_video_cc(), you.video_list())
+    print(you.video_id(), you.video_title(), you.video_author(), you.has_video_cc(), you.subtitles())
+    for i in you.video_list():
+        print(i)
