13cea67b7620f4789c62308de8c0ca279d1c96ed
metehan <mthnzbk@gmail.com>
2016-06-22 01:09:05 +0300

    altyazıların görünümü daha stabil hale getirildi.

--- pisiplayer/subtitileitem.py
+++ pisiplayer/subtitileitem.py
@@ -10,7 +10,7 @@ class SubtitleItemText(QGraphicsTextItem):
     def __init__(self, parent = None):
         super().__init__()
         self.parent = parent
-        self.timer = QTimer(self)
+
         self.font = QFont(settings().value("Player/subtitle_font") or "Noto Serif", 20)
         self.setDefaultTextColor(settings().value("Player/subtitle_color") or QColor("white"))
         self.setFont(self.font)
@@ -37,46 +37,47 @@ class SubtitleItemText(QGraphicsTextItem):
         else:
             self.subtitle_list = None
 
-    def clearHtml(self):
-        self.setPlainText("")
-        self.timer.timeout.disconnect()
-
 
     compile = re.compile(r"<(\w{1})><(\w{1})>(\w.+)<\/\w{1}><\/\w{1}>", re.S)
+    def subtitleItemParse(self, subtitle):
+        sub = self.compile.search(subtitle)
+
+        if sub:
+            self.font.setItalic(True)
+            self.font.setBold(True)
+            self.setFont(self.font)
+            self.setPlainText(sub.groups()[2])
+
+        elif subtitle.startswith("<b>"):
+            self.font.setBold(True)
+            self.font.setItalic(False)
+            self.setFont(self.font)
+            self.setPlainText(subtitle[3:].split("<")[0])
+
+        elif subtitle.startswith("<i>"):
+            self.font.setItalic(True)
+            self.font.setBold(False)
+            self.setFont(self.font)
+            self.setPlainText(subtitle[3:-4].split("<")[0])
+
+        else:
+            self.font.setBold(False)
+            self.font.setItalic(False)
+            self.setFont(self.font)
+            self.setPlainText(subtitle)
+
+        self.setPos((self.parent.size().width() - self.document().size().width()) / 2, self.parent.size().height() - 150)
+
+
     def positionValue(self, pos):
         if self.subtitle_list:
             for ctime, ltime, subtitle in self.subtitle_list:
-                if abs(pos - ctime) <= 200:
-                    self.timer.timeout.connect(self.clearHtml)
-                    sub = self.compile.search(subtitle)
-                    print(subtitle)
-                    if sub:
-                        self.font.setItalic(True)
-                        self.font.setBold(True)
-                        self.setFont(self.font)
-                        self.setPlainText(sub.groups()[2])
-
-                    elif subtitle.startswith("<b>"):
-                        self.font.setBold(True)
-                        self.font.setItalic(False)
-                        self.setFont(self.font)
-                        self.setPlainText(subtitle[3:].split("<")[0])
-
-                    elif subtitle.startswith("<i>"):
-                        self.font.setItalic(True)
-                        self.font.setBold(False)
-                        self.setFont(self.font)
-                        self.setPlainText(subtitle[3:-4].split("<")[0])
-
-                    else:
-                        self.font.setBold(False)
-                        self.font.setItalic(False)
-                        self.setFont(self.font)
-                        self.setPlainText(subtitle)
-
-                    self.setPos((self.parent.size().width() - self.document().size().width()) / 2, self.parent.size().height() - 150)
-                    self.timer.start(ltime-ctime)
+                if pos - ctime >= 100 and ltime - pos >= 100:
+                    self.subtitleItemParse(subtitle)
                     break
+
+                else:
+                    self.setPlainText("")
 
 """
 pos değeri 100ms de bir döner.
